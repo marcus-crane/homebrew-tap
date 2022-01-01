96fc2583101b91c49016951f237236ea31e38b10
Marcus Crane <marcus@utf9k.net>
2022-01-01 19:49:19 +1300

    Add intel block

--- Formula/spanner.rb
+++ Formula/spanner.rb
@@ -7,11 +7,14 @@ class Spanner < Formula
     if Hardware::CPU.arm?
       url "https://github.com/marcus-crane/spanner/releases/download/v1.0.0/spanner-v1.0.0-darwin-arm64.tar.gz"
       sha256 "108d8ec60ee92019b5ca40f477b7440008bcb52578f16ca0ad9a7fa8f33ae19f"
-
-      def install
-        bin.install "spanner"
-      end
     end
+    if Hardware::CPU.intel?
+      url "https://github.com/marcus-crane/spanner/releases/download/v1.0.0/spanner-v1.0.0-darwin-amd64.tar.gz"
+      sha256 "0872aaef4c7644b3eae974e289dd5bd25c71c1b6adf3b0b37523cbd6b816103a"
+  end
+
+  def install
+    bin.install "spanner"
   end
 
   test do
